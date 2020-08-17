8eb867347a2ea8eb3062543f25c3c2bdc7055fa4
Menendez91 <menendez91@live.com.ar>
2020-08-17 17:22:29 -0300

    BE -> FE comunicacion exitosa

--- Back_end/index.js
+++ Back_end/index.js
@@ -1,25 +1,28 @@
+
+//var bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json())
+
+
 var express = require('express');
-var cors = require('cors')
+const cors = require('cors')
 
 var app = express();
 var PORT = 3000;
 
+//ruteo 
 var routerDisp = require('./routes/dispositivo');
 var routerMedicion = require('./routes/medicion');
 var routerRiego = require('./routes/riego');
 
 app.use(express.json());
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+var corsConfig={ origin: '*', optionSucessStatus:200 };
+app.use(cors(corsConfig));
 
 app.use('/dispositivo', routerDisp);
 app.use('/medicion', routerMedicion);
 app.use('/riego', routerRiego);
-
-var corsConfig={ origin: '*', optionSucessStatus:200 };
-app.use(cors(corsConfig));
 
 app.listen(PORT, function(req, res) {
     console.log("API Funcionando ");
